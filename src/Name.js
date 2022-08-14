a7c65ca38dc369a0fe4adf47c5388839daccde5e
steph juma <stephenjuma500@gmail.com>
2022-08-14 18:14:34 +0300

    error handling:

--- src/Name.js
+++ src/Name.js
@@ -3,6 +3,8 @@ import React, { useState } from 'react'
 export default function Name() {
 
     var user = "something";
+    var userName = "something";
+    var foundUser = false;
 
     // get items in local storage and store in chat array
     var chat = JSON.parse(localStorage.getItem("chat"));
@@ -20,12 +22,14 @@ export default function Name() {
         // get the name of the user
         const name = document.getElementById("name").value;
 
-        //find if user with the name exists in the chat array
-        const foundUser = chat.find(user => user.user === name);
+        // find if user with the name exists in the chat array
+        user = chat.find(user => user.user === name);
+
 
         // if user exists, set the user to the found user
-        if (foundUser) {
-            user = foundUser.user;
+        if (user) {
+            userName = user.user
+            foundUser = true;
         } else {
             alert("User not found");
         }
@@ -34,6 +38,7 @@ export default function Name() {
     //save the message to the chat array
     const saveMessage = () => {
 
+        if (foundUser) {
         // get the message from the input field
         const message = document.getElementById("message").value;
 
@@ -44,6 +49,10 @@ export default function Name() {
         localStorage.setItem("chat", JSON.stringify(chat));
 
         alert("Message saved");
+        } else {
+            alert ("The user you entered does not exist, please select an existing user");
+        }
+
 
         // var chat = JSON.parse(localStorage.getItem("chat"));
     }
