7870cb07785e7feff8512c54fdba70e97947b23d
steph juma <stephenjuma500@gmail.com>
2022-08-14 14:28:57 +0300

    added method to find user name and look throught the existing names for the chats

--- src/Name.js
+++ src/Name.js
@@ -1,7 +1,46 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function Name() {
-  return (
-    <div>Something</div>
-  )
+    var chat = [
+        {
+            messageId: 1,
+            message: "Hello"
+        }
+    ];
+
+    const [chats, setChats] = useState([
+        { name: "John", chats: chat },
+    ])
+
+    const findName = (event)=>{
+        // get the name of the user
+        const name = document.getElementById("name").value;
+        console.log(name);
+        console.log(chats);
+        // find the users chat in the chats array
+        const user = chats.find(user => user.name === name);
+
+        if(user){
+            // if the user is found, set the chats to the user's chats
+            setChats(user.chats);
+        } else {
+            //display error message
+            alert("User not found");
+        }
+        
+        console.log(user);
+    };
+
+
+
+
+    return (
+        <>
+            <div>
+                <h1>Name</h1>
+                <input type="text" id="name"/>
+                <button onClick={findName}>Submit</button>
+            </div>
+        </>
+    )
 }
